a0e394fa76d67ef41b9cfaa85937105db0d786ed
Emilio Ormeño <eormeno@gmail.com>
2026-09-22 00:16:10 -0300

    feat: update language files and sync command in start script

--- start.sh
+++ start.sh
@@ -49,8 +49,9 @@ if [[ "$*" == *"-r"* ]]; then
     fi
 
     php artisan migrate --force
-    php artisan usim:install --no-interaction || exit 1
-    php artisan db:seed --no-interaction
+    php artisan usim:sync
+    # php artisan usim:install --no-interaction || exit 1
+    # php artisan db:seed --no-interaction
 fi
 
 # Check if port 8000 is already in use (server already running)
